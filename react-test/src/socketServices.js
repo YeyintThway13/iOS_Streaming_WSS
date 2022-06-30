31305cbb3da8ace6ce5f487deb90661631f9c6a9
Yeyint Thway <yeyint.thway@brillar.io>
2022-06-30 22:03:15 +0630

    socket io add

--- react-test/src/socketServices.js
+++ react-test/src/socketServices.js
@@ -8,24 +8,31 @@ socket.on("connection", () => {
 
 export const getFromIOSDeviceStream = (canvas, BLANK_IMG, context) => {
   socket.on("ios_device_stream", (data) => {
-    console.log("Here");
-    let blob = new Blob([data], { type: "image/jpeg" });
-    const URL = window.URL || window.webkitURL;
-    let img = new Image();
-    let u;
-    img.onload = function () {
-      console.log(img.width, img.height);
-      canvas.width = img.width;
-      canvas.height = img.height;
-      context.drawImage(img, 0, 0);
-      img.onload = null;
-      img.src = BLANK_IMG;
-      img = null;
-      u = null;
-      blob = null;
-    };
-    u = URL.createObjectURL(blob);
-    img.src = u;
+    try {
+      let blob = new Blob([data], { type: "image/jpeg" });
+      const URL = window.URL || window.webkitURL;
+      let img = new Image();
+      let u;
+      img.onload = function () {
+        try {
+          console.log(img.width, img.height);
+          canvas.width = img.width;
+          canvas.height = img.height;
+          context.drawImage(img, 0, 0);
+          img.onload = null;
+          img.src = BLANK_IMG;
+          img = null;
+          u = null;
+          blob = null;
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      u = URL.createObjectURL(blob);
+      img.src = u;
+    } catch (error) {
+      console.log(error);
+    }
   });
 };
 
